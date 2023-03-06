daba1590832c9202d209dba29060f86c045c7a2e
JUAN VITOR TASCA DE ANDRADE <juan.4910@aluno.pr.senac.br>
2023-03-06 09:00:13 -0300

    nomes

---
+++ tarefa2.php
@@ -0,0 +1,14 @@
+<?php
+    $nomeslegais = array ("João, Carlos, Ludiminiviniano, Henrique, Joaquim, Predo");
+    $carrosbacanas = array ("Fiesta, Fiat 147, Peugeot, Honda Civic, Celta");
+    $paisesinteressantes = array ("Brasil - Brasília, Rússia - Moscou, Itália - Roma, EUA - Washington D.C.");
+
+    echo $nomeslegais[0];
+    echo ("</br>");
+    echo $carrosbacanas[0];
+    echo ("</br>");
+    echo $paisesinteressantes[0];
+ 
+
+
+?>
