e48ce2097ff2402a0e73abc307138ed19ffb2524
JUAN VITOR TASCA DE ANDRADE <juan.4910@aluno.pr.senac.br>
2023-03-06 08:41:39 -0300

    alinephp

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    $nome = "Aline";
+    $idade = "23";
+    $endereco = "Rua das Flores, 316 - Centro";
+    $cidade = "Ivaiporã";
+    $cep = "86.870-000";
+    $ps = 8;
+    $ss = 10;
+    $ts = 9;
+    $media = ($ps+$ss+$ts)/3; ,
+    
+    echo("Deu certo pra caramba.");
+    echo ("</br>");
+    echo ("O seu nome é: ") . $nome;
+    echo ("</br>");
+    echo ("A sua idade é: ") . $idade;
+    echo ("</br>");
+    echo ("O seu endereço é: ") . $endereco;
+    echo ("</br>");
+    echo ("A sua cidade é: ") . $cidade;
+    echo ("</br>");
+    echo ("O seu CEP é: ") . $cep;
+    echo ("</br>");
+    echo ("Primeiro semestre: ") . $ps;
+    echo ("</br>");
+    echo ("Segundo semestre: ") . $ss;
+    echo ("</br>");
+    echo ("Terceiro semestre: ") . $ts;
+    echo ("</br>");
+    echo ("Sua média é: ") . $media;
+?>
